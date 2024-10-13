340adc95046681e0223c070c6c0457f271e40f75
RybazPolski <julorybarczyk@gmail.com>
2024-10-13 23:42:13 +0200

    Added program point's card component

--- bday-invite/app/page.tsx
+++ bday-invite/app/page.tsx
@@ -1,5 +1,6 @@
 import BackgroundImage from "@/components/BackgroundImage";
 import Counter from "@/components/Counter";
+import ProgramCard from "@/components/ProgramCard";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 
@@ -15,20 +16,17 @@ export default function Home() {
           <div className="absolute bottom-full inset-x-0 m-auto text-white text-center">Tu zaczynamy:</div>
           <Card className="absolute bottom-0 inset-x-0 h-full aspect-[5/4] bg-[url('https://wiki.openstreetmap.org/w/images/thumb/6/69/Baclaran_and_Pasay_Rotunda_-_in_Standard_map_layer.png/250px-Baclaran_and_Pasay_Rotunda_-_in_Standard_map_layer.png')] bg-cover bg-center m-auto"></Card>
         </div>
-        <div className="relative col-start-4 col-end-9 row-start-4 row-end-8">
+          <div className="relative col-start-4 col-end-9 row-start-4 row-end-8">
           <h1 className="text-3xl font-bold italic text-white">&lt;Nickname&gt;, we&apos;ve got a&nbsp;quest for you!</h1>
         </div>
         <div className="relative col-start-2 col-end-4 row-start-7 row-end-11">
-        <Card id="questThumb" className="h-full aspect-square bg-[url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5e3ccc22-bd54-4a4d-9b30-0fe3538da5ab/dbuvk4o-35a25bf5-6a65-425f-9698-82e08e85f057.jpg/v1/fit/w_800,h_655,q_70,strp/bounty_listing_by_markbulahao_dbuvk4o-414w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NjU1IiwicGF0aCI6IlwvZlwvNWUzY2NjMjItYmQ1NC00YTRkLTliMzAtMGZlMzUzOGRhNWFiXC9kYnV2azRvLTM1YTI1YmY1LTZhNjUtNDI1Zi05Njk4LTgyZTA4ZTg1ZjA1Ny5qcGciLCJ3aWR0aCI6Ijw9ODAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.cIMGuhthZ1mNx26osRf32CLhZUCTUVN5sDcm6ZCorqs')] bg-cover bg-center m-auto text-center align-middle text-white font-semibold text-2xl">Quest</Card>
-        <div className="my-auto text-center text-2xl text-white">16:00</div>
+          <ProgramCard id="questThumb" title="Quest" imageUrl="/quest/01.jpg" time="16:00"/>
         </div>
         <div className="relative col-start-7 col-end-9 row-start-8 row-end-12">
-          <Card id="lasertagThumb" className="h-full aspect-square bg-[url('https://lh3.googleusercontent.com/p/AF1QipN1QuDp_VHkD8OTdkMVSwXz-XSM0srNiEGKh1NY=s1360-w1360-h1020')] bg-cover bg-center m-auto text-center align-middle text-white font-semibold text-2xl">Lasertag</Card>
-          <div className="my-auto text-center text-2xl text-white">17:00</div>
+          <ProgramCard className="relative -top-3" id="laserarenaThumb" title="Lasertag" imageUrl="/laserarena/01.jpg" time="17:00"/>
         </div>
         <div className="relative col-start-10 col-end-12 row-start-2 row-end-6">
-          <Card id="lokalThumb" className="h-full aspect-square bg-[url('https://party-poznan.pl/wp-content/uploads/2021/09/partyhouse_01-27.jpg')] bg-cover bg-center m-auto text-center align-middle text-white font-semibold text-2xl">Lokal</Card>
-          <div className="my-auto text-center text-2xl text-white">18:45</div>
+          <ProgramCard className="relative -top-[4vh] left-[4vw]" id="partyhouseThumb" title="Lokal" imageUrl="/partyhouse/salon/01.jpg" time="18:45"/>
         </div>
         <div className="relative col-start-1 col-end-7 row-start-12 row-end-13">
           <h3 className="text-center text-xl overflow-show text-white text-nowrap">Motyw imperzy: &quot;I&apos;m ready for the quest!&quot;</h3>
---
+++ bday-invite/components/ProgramCard.tsx
@@ -0,0 +1,19 @@
+import { Card, CardTitle } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+
+export default function ProgramCard({id, className, title, imageUrl, time}:{id?:string, className?:string, title:string,imageUrl:string,time:string}){
+    return (
+        <>
+            <span className={className}>
+            <Card id={id}
+                style={{ '--image-url': `url(${imageUrl})` }} 
+                className={`h-full aspect-square bg-[url:var(--image-url)] bg-cover bg-center m-auto text-center align-middle text-white font-semibold text-2xl`}
+                title={title}
+                >
+                    <CardTitle>{title}</CardTitle>
+                </Card>
+                <div className="my-auto text-center text-2xl text-white">{time}</div>
+            </span>
+        </>
+    )
+}
