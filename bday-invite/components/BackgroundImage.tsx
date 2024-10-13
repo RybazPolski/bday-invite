83f4dfad8a09b2665a370e6bc7b5b28db2b7e345
RybazPolski <julorybarczyk@gmail.com>
2024-10-13 23:39:52 +0200

    BackgroundImage fix

--- bday-invite/components/BackgroundImage.tsx
+++ bday-invite/components/BackgroundImage.tsx
@@ -2,13 +2,9 @@ import Image from "next/image"
 
 export default function BackgroundImage({team, quest}:{team?:"orange"|"lime"|undefined,quest?:"accepted"|"denied"|undefined}){
     return (
-        <Image 
-            className="fixed -z-100 w-full h-full blur-sm"
-            src={`/backgrounds/${quest=="accepted"?`cantina${team==undefined?"":"-"+team}`:quest=="denied"?"ship":"sewers"}.jpg`}
-            layout="fill"
-            objectFit="cover"
-            objectPosition="center"
-            alt={`${quest=="accepted"?`cantina${team==undefined?"":"-"+team}`:quest=="denied"?"ship":"sewers"} background`}
+        <div
+            style={{'--image-url':`url('/backgrounds/${quest=="accepted"?`cantina${team==undefined?"":"-"+team}`:quest=="denied"?"ship":"sewers"}.jpg')`}}
+            className="bg-[url:var(--image-url)] bg-cover bg-center fixed -z-100 w-full h-full blur-sm"
         />
     )
 }
