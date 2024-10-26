d0c86dbf77679bac8a09398eb2b883b1700f9564
RybazPolski <julorybarczyk@gmail.com>
2024-10-26 19:22:39 +0200

    Add border to program card

--- bday-invite/components/ProgramCard.tsx
+++ bday-invite/components/ProgramCard.tsx
@@ -5,15 +5,21 @@ export default function ProgramCard({id, className, title, imageUrl, time}:{id?:
     return (
         <>
             <span className={className}>
-            <Card id={id}
-                style={{ '--image-url': `url(${imageUrl})` }} 
-                className={`h-full aspect-square bg-[url:var(--image-url)] bg-cover bg-center m-auto text-center align-middle text-white font-semibold text-2xl`}
-                title={title}
-                >
-                    <CardTitle>{title}</CardTitle>
-                </Card>
-                <div className="my-auto text-center text-2xl text-white">{time}</div>
+                <span className="relative block mx-auto -top-2
+                aspect-square h-full overflow-hidden rounded-lg p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+                    <span className="absolute inset-[-100%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+                            <Card id={id}
+                            style={{ '--image-url': `url(${imageUrl})` }} 
+                            className={`h-full aspect-square bg-[url:var(--image-url)] bg-cover bg-center m-auto text-center align-middle text-white font-semibold text-2xl 
+                                rounded-lg bg-slate-950  backdrop-blur-3xl`}
+                            title={title}
+                            >
+                                <CardTitle>{title}</CardTitle>
+                            </Card>
+                    </span>
+                <div className="relative -top-2 mx-auto text-center text-2xl text-white">{time}</div>
             </span>
         </>
     )
 }
+
